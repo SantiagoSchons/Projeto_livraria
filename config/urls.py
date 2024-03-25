a7c7d32c0dc79754eb7d3732b321a57779b11efd
Santiago de Oliveira <santiagodeoliveira2004@gmail.com>
2024-03-25 09:25:05 -0300

    Reestruturação em pastas de models, views e serializers

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
-
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
@@ -12,6 +12,8 @@ router.register(r"autores", AutorViewSet)
 router.register(r"livros", LivroViewSet)
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('admin/', admin.site.urls),
     path("", include(router.urls)),
 ]
