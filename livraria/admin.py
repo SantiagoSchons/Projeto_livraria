2d22989b566924492fcb57f82ea528c1e44da263
Santiago de Oliveira <santiagodeoliveira2004@gmail.com>
2024-03-26 12:18:38 -0300

    Criação da entidade Compra integrada ao usuário do projeto

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Categoria, Editora, Autor, Livro
+from .models import Categoria, Editora, Autor, Livro, Compra
 
 # admin.site.register(Categoria)
 # admin.site.register(Editora)
 # admin.site.register(Autor)
 # admin.site.register(Livro)
+
+admin.site.register(Compra)
 
 @admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
