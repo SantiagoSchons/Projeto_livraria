21c12c11e77e7c9d46dbc4b39eb27dc66940ab75
Santiago de Oliveira <santiagodeoliveira2004@gmail.com>
2024-03-26 12:43:01 -0300

    Visualização dos itens da compra no endpoint de listagem de compras1

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,10 +1,16 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = "__all__"
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
-    
